4f5254fbbd6ad235af2ee03ec2f8f4c63f3ca70d
Sibylle <sibylle.hervouet@hetic.net>
2019-04-17 19:05:21 +0200

    commit.mercredisoir

--- scripts/main.js
+++ scripts/main.js
@@ -1,39 +1,100 @@
+// let level1, level2, level3
+//
+//
+// let chooseLevel = {
+//   words1: ['PIRATE', 'BATEAU', 'CANON', 'CALE', 'CAPE', 'MARIN', 'PERROQUET', 'PONT', 'POUPE', 'NAVIRE', 'CAPITAINE', 'TRÉSOR', 'RAME', 'BOTTES', 'CRANE', 'RAT', 'ILE','PORT','SABRE', 'JACK', 'PLUME'],
+//   words2: ['GOUVERNAIL', 'ESCALE', 'PAVILLON', 'CORSAIRE', 'TRIBORD','AMARRER', 'NOEUD', 'VAISSEAU', 'CANONNER', 'BOUCANIER', 'CONTREBANDE', 'TRICORNE', 'RHUM', 'TONNEAU', 'TEMPETE', 'MONSTRE', 'VOLEUR'],
+//   words3: ['MOUSAILLON', 'FLIBUSTIER', 'MARCHEPIED', 'SABORDER', 'ABORDAGE', 'BABORD', 'FREGATE', 'GALION', 'CHALOUPE', 'PALMIER', 'MARCHANDISE', 'PLANCHE', 'CABINE', 'CAPITAINE', 'CARAIBES', 'BERMUDE', 'PISTOLET','MEDAILLON','CANNIBALE'],
+//   level1: level1,
+//   level2: level2,
+//   level3: level3,
+//   levelChoice = function(){
+//     this.level1=document.getElementbyId('level1')
+//     this.level2=document.getElementbyId('level2')
+//     this.level3=document.getElementbyId('level3')
+//     this.level1.addEventListener(
+//       'click',
+//       function(){
+//         promptWord.words=this.level1
+//       })
+//     this.level2.addEventListener(
+//       'click',
+//       function(){
+//         promptWord.words=this.level2
+//       })
+//     this.level3.addEventListener(
+//       'click',
+//       function(){
+//         promptWord.words=this.level3
+//         })
+//   }
+// }
+//
+// let replayButton
+//
+
 let game ={
   score: 0,
-  // replay: function(){
-  //   game.winCheck=document.getElementsByClassName('.txtLetter')
-  //   if(game.winCheck.classList.contains('display')||game.looseLetter=10){
-  //   end=true
-  //   console.log(nbSameLetter)
-  //  }
-  // },
-}
-
-let pos, secretWord, txtLetter, divLetter, wordSpace, alphabet, alphaPos, tempLetter, isIn, nbSameLetter, backgroundImage, winCheck
-
-let promptWord = {
-  words : ['PIRATE', 'BATEAU', 'CANON'],
-  pos : pos,
-  secretWord : secretWord,
-  looseLetter : 0,
+  replayButton: document.getElementById('replay'),
+  levels: document.querySelectorAll(".level"),
   end: false,
+    // game.replayButton.addEventListener(
+    //   'click',
+    //   location.reload (),
+    //    false
+    // )
+   }
+
+
+let pos, secretWord='test', txtLetter, divLetter, wordSpace, alphaPos, tempLetter, isIn, nbSameLetter, backgroundImage, winCheck, words, tempLevel
+
+
+let promptWord = { //Definir le mot secret à efficher (nombre de cases, etc.)
+  level1: document.querySelector('#level1'),
+  level2: document.querySelector('#level2'),
+  level3: document.querySelector('#level3'),
+  tempLevel: tempLevel, //variable qui enregistre le choix du tableau
+  words:[
+    ['PIRATE', 'BATEAU', 'CANON', 'CALE', 'CAPE', 'MARIN', 'PERROQUET', 'PONT', 'POUPE', 'NAVIRE', 'CAPITAINE', 'TRÉSOR', 'RAME', 'BOTTES', 'CRANE', 'RAT', 'ILE','PORT','SABRE', 'JACK', 'PLUME'],
+  ['GOUVERNAIL', 'ESCALE', 'PAVILLON', 'CORSAIRE', 'TRIBORD','AMARRER', 'NOEUD', 'VAISSEAU', 'CANONNER', 'BOUCANIER', 'CONTREBANDE', 'TRICORNE', 'RHUM', 'TONNEAU', 'TEMPETE', 'MONSTRE', 'VOLEUR', 'PIEUVRE'],
+  ['MOUSAILLON', 'FLIBUSTIER', 'MARCHEPIED', 'SABORDER', 'ABORDAGE', 'BABORD', 'FREGATE', 'GALION', 'CHALOUPE', 'PALMIER', 'MARCHANDISE', 'PLANCHE', 'CABINE', 'CAPITAINE', 'CARAIBES', 'BERMUDE', 'PISTOLET','MEDAILLON','CANNIBALE']],//tableau qui contient les different niveaux de difficulte
+  pos : pos, //position du mot à trouver dans le tableau
+  secretWord : secretWord, //mot à trouver
+  looseLetter : 0, //mauvaise lettre //
   boxGame:[],
-  txtLetter: txtLetter,
-  divLetter: divLetter,
+  txtLetter: txtLetter,// les li qui contiennent les lettres de l'alphabet
+  divLetter: divLetter, //ul qui contient les li qui contiennent les lettre de l'alphabet
   wordSpace: wordSpace,
-  alphabet: alphabet,
-  alphaPos: alphaPos,
+  alphabet: [], //tableau qui contient les lettres de l'alphabet
+  alphaPos: alphaPos,//position de la letter dans l'alphabet
   tempLetter: tempLetter,
-  isIn: isIn,
-  nbSameLetter: nbSameLetter,
-  backgroundImage: backgroundImage,
-  winCheck: winCheck,
-  randomSecretWord: function(){
-    this.pos=Math.floor(Math.random()*this.words.length) //choix aléatoire dans words selon la position des elmts
-    this.secretWord=this.words[pos]
+  isIn: isIn, //variable qui enregistre si a lettre sur laquelle on a cliqué est dans le mot secret
+  nbSameLetter: nbSameLetter,// nb de lettres identiques ds un même mot
+  backgroundImage: backgroundImage, //image de fond qui change selon les erreures
+  chooseLevel: function(){
+    //choisir la difficulté
+    this.level1.addEventListener(
+      "click",
+      function(){
+        this.tempLevel=0
+      })
+    this.level2.addEventListener(
+      "click",
+      function(){
+        this.tempLevel=1
+      })
+    this.level3.addEventListener(
+      "click",
+      function(){
+        this.tempLevel=2
+      })
   },
-  createBox: function(divLetter){
-    this.wordSpace=document.getElementsByClassName('wordSpace')
+  randomSecretWord: function(){ //génére un mot aleatoire selon les 3 niveaux de difficulté
+    this.pos=Math.floor(Math.random()*this.words.length) //choix aléatoire dans words selon la position des elmts
+    this.secretWord=this.words[this.tempLevel][this.pos]//le secret word est defini selon le niveau et un nombre aleatoire
+  },
+  createBox: function(divLetter){// créé les emplacements par lettre
+    this.wordSpace=document.querySelector('.wordSpace')
     this.divLetter=document.createElement('ul') //on créé un ul .divLetter dans la div .wordSpace
     this.divLetter.className='divLetter'
     this.wordSpace.appendChild(this.divLetter)
@@ -46,7 +107,7 @@ let promptWord = {
       this.txtLetter.innerHTML=this.boxGame[i]
     }
   },
-  runAlphabet: function(){
+  runAlphabet: function(){//parcours l'alphabet pour vérifier si la lettre cliquée est présente dans le mot secret
     for (let i=0; i < this.alphabet.length; i++){ //on parcourt alphabet
       console.log(this.alphabet[i].innerHTML)
       this.alphaPos = this.alphabet[i]
@@ -57,16 +118,45 @@ let promptWord = {
           this.isIn=this.secretWord.lastIndexOf(this.tempLetter)//si la lettre sur laquelle on a cliqué (lastIndexOf) est dans le mot (secretWord)
           this.nbSameLetter = document.querySelectorAll('[data-letter='+this.tempLetter+']')
           if(game.isIn!=-1){ //si isIn est vrai alors
-            for(let i=0; i<this.nbSameLetter.length; i++)
+            for(let i=0; i<this.nbSameLetter.length; i++)//vérifie si la même lettre est présente plusieurs fois dans le même mot
             {
-              this.nbSameLetter[i].classList.add('display')
+              this.nbSameLetter[i].classList.add('display')// si la lettre est présente, elle apparaît
             }
           }
           else{
             this.looseLetter++ //le compteur looseLetter prend la valeur + 1
             console.log(this.looseLetter);
-            this.backgroundImage=document.getElementsByClassName('gameSpace')
-            this.backgroundImage.style.background="url(../images/fondAccueil)"
+            this.backgroundImage=document.querySelector('gameSpace')
+            if(this.looseLetter==1){
+              this.backgroundImage.style.background="url(../images/fondAccueil1)"
+            }
+            else if(this.looseLetter==2){
+              this.backgroundImage.style.background="url(../images/fondAccueil2)"
+            }
+            else if(this.looseLetter==3){
+              this.backgroundImage.style.background="url(../images/fondAccueil3)"
+            }
+            else if(this.looseLetter==4){
+              this.backgroundImage.style.background="url(../images/fondAccueil4)"
+            }
+            else if(this.looseLetter==5){
+              this.backgroundImage.style.background="url(../images/fondAccueil5)"
+            }
+            else if(this.looseLetter==6){
+              this.backgroundImage.style.background="url(../images/fondAccueil6)"
+            }
+            else if(this.looseLetter==7){
+              this.backgroundImage.style.background="url(../images/fondAccueil7)"
+            }
+            else if(this.looseLetter==8){
+              this.backgroundImage.style.background="url(../images/fondAccueil8)"
+            }
+            else if(this.looseLetter==9){
+              this.backgroundImage.style.background="url(../images/fondAccueil9)"
+            }
+            else{
+              this.backgroundImage.style.background="url(../images/fondAccueil10)"
+            }
           }
 
       })
@@ -74,102 +164,7 @@ let promptWord = {
   }
 }
 
-
+promptWord.chooseLevel()
 promptWord.randomSecretWord()
 promptWord.createBox()
 promptWord.runAlphabet()
-
-
-
-
-
-
-
-
-
-
-/*
-
-// Fonction quand on clique sur une des lettres
-
-//pour chaque lettre du mot, créer un ediv qui contient un formulaire
-//si la lettre de l'alphabet est dans le mot, alors on affiche la lettre
-
-var motSecret;
-
-                var now=new Date();            // Date d'aujourd'hui
-                var tableauMot=new Array();    // Le tableau qui contient les lettres du mot à trouver
-                var mots=new Array();        // Le tableau qui contient tous les mots possibles
-
-                var tailleMot;                // Le nombre de lettres du mot à trouver
-                var coupsManques=0;            // Le nombre de lettres fausses essayées
-                var lettresTrouvees=0;        // Le nombre de lettres trouvées
-                var fini=false;                // true si le jeu est terminé
-
-                mots[0]="TITANESQUE";
-                mots[1]="ANABELLE";
-                mots[2]="POMPIER";
-                mots[3]="OBELISQUE";
-                mots[4]="PLACARD";
-                mots[5]="RADIATEUR";
-                mots[6]="DEODORANT";
-                mots[7]="CAMION";
-                mots[8]="HORLOGE";
-                mots[9]="MARGUERITE";
-                mots[10]="ELEPHANT";
-                mots[11]="IGLOO";
-                mots[12]="NAVIRE";
-
-                // On prend un mot au hasard en fonction de la seconde en cours
-                motSecret=mots[now.getSeconds() % mots.length];
-                tailleMot=motSecret.length;
-
-                // Permet de changer la couleur des touches du clavier
-                function changeCouleur(element,couleur){
-                    element.style.backgroundColor=couleur;
-                }
-
-                // Gère tous les traitements à faire lorsqu'on appuie sur une touche
-                function proposer(element){
-
-                    // Si la couleur de fond est lightgreen, c'est qu'on a déjà essayé - on quitte la fonction
-                    if(element.style.backgroundColor=="lightGreen" ||fini) return;
-
-                    // On récupère la lettre du clavier et on met la touche en lightgreen (pour signaler qu'elle est cliquée)
-                    var lettre=element.innerHTML;
-                    changeCouleur(element,"lightGreen");
-
-                    // On met la variable trouve à false;
-                    var trouve=false;
-
-                    // On parcours chaque lettre du mot, on cherche si on trouve la lettre séléectionnée au clavier
-                    for(var i=0; i<tailleMot; i++) {
-
-                        // Si c'est le cas :
-                        if(tableauMot[i].innerHTML==lettre) {
-                            tableauMot[i].style.visibility='visible';    // On affiche la lettre
-                            trouve=true;
-                            lettresTrouvees++;
-                        }
-
-                    }
-
-                    // Si la lettre n'est pas présente, trouvé vaut toujours false :
-                    if(!trouve){
-                        coupsManques++;
-                        document.images['pendu'].src="images/pendu_"+coupsManques+".jpg"; // On change l'image du pendu
-
-                        // Si on a raté 9 fois :
-                        if(coupsManques==9){
-                            alert("Vous avez perdu !");
-                            for(var i=0; i<tailleMot; i++) tableauMot[i].style.visibility='visible';
-                            fini=true;
-                            // on affiche le mot, on fini le jeu
-                        }
-                    }
-                    if(lettresTrouvees==tailleMot){
-                        alert("Bravo ! Vous avez découvert le mot secret !");
-                        fini=true;
-                    }
-                }
-*/
