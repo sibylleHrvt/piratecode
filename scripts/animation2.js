902b3d7f29e16a6eadfea2c71d216ffb375a8ded
Sibylle <sibylle.hervouet@hetic.net>
2019-04-18 10:21:58 +0200

    comiitJeudiMatin

--- scripts/animation2.js
+++ scripts/animation2.js
@@ -1,4 +1,4 @@
-const maxWidth = 1400
+const maxWidth = 830
 let step = 0
 let bateauNav = document.querySelector('.bateauNav')
 
@@ -8,9 +8,36 @@ let bateauNav = document.querySelector('.bateauNav')
           <!-- step = step-1 -->
           <!-- bateauPos.style.right = step + 'px' -->
           if (step<maxWidth) {
-            step = step +1
+            step = step + 1
             bateauNav.style.left = step + 'px'
           }
           },
           50
       )
+
+// let move, step, bateauPos
+//
+// let boatAnimation={
+//   maxWidth: 830,
+//   step: 0,
+//   bateauaAv: document.querySelector('.bateauNav'),
+//   move: move,
+//   step: step,
+//   bateauPos: bateauPos,
+//   movingBoat: function(){
+//     this.move=setInterval(
+//       function(){
+//
+//         <!-- this.step = this.step-1 -->
+//         <!-- this.bateauPos.style.right = this.step + 'px' -->
+//         if (this.step<this.maxWidth) {
+//           this.step = this.step + 1
+//           this.bateauNav.style.left = this.step + 'px'
+//         }
+//         },
+//         50
+//     )
+//   }
+// }
+//
+// boatAnimation.movingBoat()
